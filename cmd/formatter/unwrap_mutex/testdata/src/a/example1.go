ba5057963ee36c5c98625b28f939c707b9eb936d
Siza1512 <Ivan.sizow2017@yandex.ru>
2022-06-25 18:12:37 +0700

    clear

--- cmd/formatter/unwrap_mutex/testdata/src/a/example1.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "sync"
-
-func main() {
-	var mu sync.Mutex
-	mu.Lock()
-	defer mu.Unlock()
-}
--- cmd/formatter/unwrap_mutex/testdata/src/b/example1.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "sync"
-
-func main() {
-	mu := new(sync.Mutex) // want "unwrap_mutex"
-	mu.Lock()
-	defer mu.Unlock()
-}
