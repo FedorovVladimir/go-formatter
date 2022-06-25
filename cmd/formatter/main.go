437bf72ed9d50bdaf0f7a6f2d50eaa773f16afb3
Siza1512 <Ivan.sizow2017@yandex.ru>
2022-06-25 14:07:19 +0700

    on formatter

--- cmd/formatter/main.go
+++ cmd/formatter/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"go-formatter/cmd/formatter/empty_func_body"
+	"go-formatter/cmd/formatter/grouped_vars"
 	"go-formatter/cmd/formatter/many_arguments"
+	"go-formatter/cmd/formatter/with"
+
 	"golang.org/x/tools/go/analysis/multichecker"
 )
 
 func main() {
 	multichecker.Main(
-		//with.Analyzer,
-		//empty_func_body.Analyzer,
-		//grouped_vars.Analyzer,
+		with.Analyzer,
+		empty_func_body.Analyzer,
+		grouped_vars.Analyzer,
 		many_arguments.Analyzer,
 	)
 }
