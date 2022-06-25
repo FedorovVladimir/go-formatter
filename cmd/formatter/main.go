3faa3eb5dd139904eccf4d291a2a4bda69cc75ca
Siza1512 <Ivan.sizow2017@yandex.ru>
2022-06-25 18:25:58 +0700

    fix

--- cmd/formatter/main.go
+++ cmd/formatter/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"go-formatter/cmd/formatter/context_first_parameter"
-	"go-formatter/cmd/formatter/empty_func_body"
-	"go-formatter/cmd/formatter/grouped_vars"
-	"go-formatter/cmd/formatter/many_arguments"
-	"go-formatter/cmd/formatter/methods_with_star_and_rename"
-	"go-formatter/cmd/formatter/new_line"
-	"go-formatter/cmd/formatter/rm_ignore_vars"
-	"go-formatter/cmd/formatter/start_enums_at_one"
-	"go-formatter/cmd/formatter/with"
-
+	"git.user-penguin.space/vladimir/go-formatter/cmd/formatter/context_first_parameter"
+	"git.user-penguin.space/vladimir/go-formatter/cmd/formatter/empty_func_body"
+	"git.user-penguin.space/vladimir/go-formatter/cmd/formatter/grouped_vars"
+	"git.user-penguin.space/vladimir/go-formatter/cmd/formatter/many_arguments"
+	"git.user-penguin.space/vladimir/go-formatter/cmd/formatter/methods_with_star_and_rename"
+	"git.user-penguin.space/vladimir/go-formatter/cmd/formatter/new_line"
+	"git.user-penguin.space/vladimir/go-formatter/cmd/formatter/rm_ignore_vars"
+	"git.user-penguin.space/vladimir/go-formatter/cmd/formatter/start_enums_at_one"
+	"git.user-penguin.space/vladimir/go-formatter/cmd/formatter/with"
 	"golang.org/x/tools/go/analysis/multichecker"
 )
 
