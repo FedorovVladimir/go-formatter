ed90e94bd6be33ba3182ede9464db2e1d6bcb979
Siza1512 <Ivan.sizow2017@yandex.ru>
2022-06-25 16:11:54 +0700

    add start_enums_at_one formatter to main

--- cmd/formatter/main.go
+++ cmd/formatter/main.go
@@ -6,6 +6,7 @@ import (
 	"go-formatter/cmd/formatter/grouped_vars"
 	"go-formatter/cmd/formatter/many_arguments"
 	"go-formatter/cmd/formatter/rm_ignore_vars"
+	"go-formatter/cmd/formatter/start_enums_at_one"
 	"go-formatter/cmd/formatter/with"
 
 	"golang.org/x/tools/go/analysis/multichecker"
@@ -19,5 +20,6 @@ func main() {
 		many_arguments.Analyzer,
 		with.Analyzer,
 		rm_ignore_vars.Analyzer,
+		start_enums_at_one.Analyzer,
 	)
 }
