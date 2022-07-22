918be25a352d18ce551f4def321ad4829e38512e
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2022-07-22 12:58:18 +0700

    clear

--- formatter_order/formatter.go
+++ formatter_order/formatter.go
@@ -60,6 +60,7 @@ type fileData struct {
 	positions    []*position
 	lastPosition *position
 	currentFile  *token.File
+	comments     []*position
 }
 
 func newFileData(currentFile *token.File) *fileData {
@@ -72,6 +73,94 @@ func newFileData(currentFile *token.File) *fileData {
 	}
 }
 
+func (data *fileData) getComments(pass *analysis.Pass, file *ast.File) {
+	data.comments = make([]*position, 0, len(file.Comments))
+	for _, c := range file.Comments {
+		if pass.Fset.Position(c.Pos()).Column != 1 {
+			continue
+		}
+		data.comments = append(data.comments, &position{pos: c.Pos(), end: c.End()})
+	}
+}
+
+func (data *fileData) getPrevEnd() token.Pos {
+	if data.lastNode != nil {
+		return data.lastNode.end
+	}
+	return token.Pos(data.currentFile.Base())
+}
+
+func (data *fileData) getPos(n ast.Node, end token.Pos) token.Pos {
+	pos := n.Pos()
+	switch e := n.(type) {
+	case *ast.FuncDecl:
+		if e.Doc != nil {
+			pos = e.Doc.Pos()
+		}
+	case *ast.GenDecl:
+		if e.Doc != nil {
+			pos = e.Doc.Pos()
+		}
+	}
+	for _, comment := range data.comments {
+		if end < comment.pos && comment.pos < pos {
+			pos = comment.pos
+		}
+	}
+	return pos
+}
+
+func (data *fileData) appendFuncDecl(pos token.Pos, e *ast.FuncDecl) {
+	data.lastNode = &position{pos: pos, end: e.End(), filename: data.currentFile.Name()}
+	d := selectDeclForFunc(e.Name)
+	data.groups[d] = append(data.groups[d], data.lastNode)
+
+	data.lastPosition = &position{pos: pos, end: e.End()}
+	data.positions = append(data.positions, data.lastPosition)
+}
+
+func (data *fileData) appendGenDecl(pos token.Pos, e *ast.GenDecl) {
+	switch e.Tok {
+	case token.IMPORT:
+		data.lastNode = &position{pos: pos, end: e.End(), filename: data.currentFile.Name()}
+		data.lastPosition = &position{pos: pos, end: e.End()}
+	case token.CONST, token.VAR, token.TYPE:
+		if data.currentFile.Position(e.Pos()).Column != 1 {
+			return
+		}
+
+		data.lastNode = &position{pos: pos, end: e.End(), filename: data.currentFile.Name()}
+		data.groups[tokenToDecl[e.Tok]] = append(data.groups[tokenToDecl[e.Tok]], data.lastNode)
+
+		data.lastPosition = &position{pos: pos, end: e.End()}
+		data.positions = append(data.positions, data.lastPosition)
+	}
+}
+
+func (data *fileData) reportGroup(pass *analysis.Pass, positions []*position, i int, groups map[decl][]*position, decl decl) (int, error) {
+	if nodes, ok := groups[decl]; ok {
+		for _, node := range nodes {
+			if node.pos == positions[i].pos {
+				i++
+				continue
+			}
+
+			node.pos = token.Pos(int(node.pos) - data.currentFile.Base())
+			node.end = token.Pos(int(node.end) - data.currentFile.Base())
+			fileBytes, err := readFile(node.filename)
+			if err != nil {
+				return i, err
+			}
+			text := fileBytes[node.pos:node.end]
+
+			report(pass, positions[i].pos, positions[i].end, text, "formatter_order")
+			i++
+		}
+		return i, nil
+	}
+	return i, nil
+}
+
 func run(pass *analysis.Pass) (interface{}, error) {
 	for _, file := range pass.Files {
 		if len(file.Decls) == 0 {
@@ -79,12 +168,11 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		}
 
 		data := newFileData(pass.Fset.File(file.Decls[0].Pos()))
-		comments := getComments(pass, file)
+		data.getComments(pass, file)
 
 		for _, n := range file.Decls {
-
 			prevEnd := data.getPrevEnd()
-			pos := getPos(n, comments, prevEnd)
+			pos := data.getPos(n, prevEnd)
 
 			if data.lastNode != nil {
 				data.lastNode.end = pos - 1
@@ -95,39 +183,19 @@ func run(pass *analysis.Pass) (interface{}, error) {
 
 			switch e := n.(type) {
 			case *ast.FuncDecl:
-				data.lastNode = &position{pos: pos, end: e.End(), filename: data.currentFile.Name()}
-				d := selectDeclForFunc(e.Name)
-				data.groups[d] = append(data.groups[d], data.lastNode)
-
-				data.lastPosition = &position{pos: pos, end: e.End()}
-				data.positions = append(data.positions, data.lastPosition)
+				data.appendFuncDecl(pos, e)
 			case *ast.GenDecl:
-				switch e.Tok {
-				case token.IMPORT:
-					data.lastNode = &position{pos: pos, end: e.End(), filename: data.currentFile.Name()}
-					data.lastPosition = &position{pos: pos, end: e.End()}
-				case token.CONST, token.VAR, token.TYPE:
-					if data.currentFile.Position(e.Pos()).Column != 1 {
-						continue
-					}
-
-					data.lastNode = &position{pos: pos, end: e.End(), filename: data.currentFile.Name()}
-					data.groups[tokenToDecl[e.Tok]] = append(data.groups[tokenToDecl[e.Tok]], data.lastNode)
-
-					data.lastPosition = &position{pos: pos, end: e.End()}
-					data.positions = append(data.positions, data.lastPosition)
-				}
+				data.appendGenDecl(pos, e)
 			}
 		}
 
-		f := pass.Fset.File(data.lastPosition.pos)
-		end := token.Pos(f.Base() + f.Size())
+		end := token.Pos(data.currentFile.Base() + data.currentFile.Size())
 		data.lastPosition.end = end - 1
 		data.lastNode.end = end
 
 		i := 0
 		for _, decl := range orderDecl {
-			k, err := reportGroup(pass, data.positions, i, data.groups, decl, f)
+			k, err := data.reportGroup(pass, data.positions, i, data.groups, decl)
 			if err != nil {
 				return nil, err
 			}
@@ -136,24 +204,6 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	}
 
 	return nil, nil
-}
-
-func (data *fileData) getPrevEnd() token.Pos {
-	if data.lastNode != nil {
-		return data.lastNode.end
-	}
-	return token.Pos(data.currentFile.Base())
-}
-
-func getComments(pass *analysis.Pass, file *ast.File) []*position {
-	comments := make([]*position, 0, len(file.Comments))
-	for _, c := range file.Comments {
-		if pass.Fset.Position(c.Pos()).Column != 1 {
-			continue
-		}
-		comments = append(comments, &position{pos: c.Pos(), end: c.End()})
-	}
-	return comments
 }
 
 func selectDeclForFunc(name *ast.Ident) decl {
@@ -168,50 +218,6 @@ func selectDeclForFunc(name *ast.Ident) decl {
 		return publicFuncDecl
 	}
 	return privateFuncDecl
-}
-
-func getPos(n ast.Node, comments []*position, end token.Pos) token.Pos {
-	pos := n.Pos()
-	switch e := n.(type) {
-	case *ast.FuncDecl:
-		if e.Doc != nil {
-			pos = e.Doc.Pos()
-		}
-	case *ast.GenDecl:
-		if e.Doc != nil {
-			pos = e.Doc.Pos()
-		}
-	}
-	for _, comment := range comments {
-		if end < comment.pos && comment.pos < pos {
-			pos = comment.pos
-		}
-	}
-	return pos
-}
-
-func reportGroup(pass *analysis.Pass, positions []*position, i int, groups map[decl][]*position, decl decl, f *token.File) (int, error) {
-	if nodes, ok := groups[decl]; ok {
-		for _, node := range nodes {
-			if node.pos == positions[i].pos {
-				i++
-				continue
-			}
-
-			node.pos = token.Pos(int(node.pos) - f.Base())
-			node.end = token.Pos(int(node.end) - f.Base())
-			fileBytes, err := readFile(node.filename)
-			if err != nil {
-				return i, err
-			}
-			text := fileBytes[node.pos:node.end]
-
-			report(pass, positions[i].pos, positions[i].end, text, "formatter_order")
-			i++
-		}
-		return i, nil
-	}
-	return i, nil
 }
 
 func report(pass *analysis.Pass, pos token.Pos, end token.Pos, text []byte, msg string) {
